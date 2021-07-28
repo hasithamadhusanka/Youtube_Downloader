789dbb5dbf315396876286498029d7d987f5d1d5
hasithamadhusanka <86175258+hasithamadhusanka@users.noreply.github.com>
2021-07-28 09:47:33 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Name     : inline-tube-mate [ Telegram ]
-# Repo     : https://github.com/m4mallu/inine-tube-mate
-# Author   : Renjith Mangal [ https://t.me/space4renjith ]
-# Credits  : https://github.com/SpEcHiDe/AnyDLBot
+# Repo     : https://github.com/hasithamadhusanka/Youtube_Downloader
+# Author   : Hasitha Madhushanka
+# Credits  : https://github.com/hasithamadhusanka/Youtube_Downloader
 
 
 import os
